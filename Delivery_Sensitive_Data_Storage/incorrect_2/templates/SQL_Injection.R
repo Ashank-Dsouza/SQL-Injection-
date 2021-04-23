0e18a1959763af9cf5ab1d572087dcaf9498ee66
Ashank-Dsouza <38183823+Ashank-Dsouza@users.noreply.github.com>
2021-04-23 15:59:43 +0530

    corrects

--- Delivery_Sensitive_Data_Storage/incorrect_2/templates/SQL_Injection.R
+++ Delivery_Sensitive_Data_Storage/incorrect_2/templates/SQL_Injection.R
@@ -2,32 +2,30 @@ install.packages("RMySQL")
 library(RMySQL)
 
 
-mydb = dbConnect(MySQL(), user='root',
+mydb = dbConnect(MySQL(), 
+                 user='root',
                  password='3216054regulas',
                  dbname='Users',
                  host='localhost')
 
-name <- readline(prompt="Enter name: ")
+name <- readline(prompt = Select_command )
 
 print(name)
 
-letters_only <- function(x) !grepl("[^A-Za-z]", x)
-
-if(letters_only(name)){
-  Select_command <- "select * from Information Where Name = "
+Select_command <- "select * from Information Where Name = "
   
-  nameField <- paste("'", name, "'", sep = "")
+if(grep("[:alpha:]", name)){
   
-  txtSql  <- paste(Select_command, nameField, sep ="")
-  
-  print(txtSql)
-  
-  rs = dbSendQuery(mydb, txtSql)
-  
-  data = fetch(rs, n=-1)
-  print(data) 
-  
+    txtSql  <- paste(Select_command, name, sep ="")
+    
+    print(txtSql)
+    
+    rs = dbSendQuery(mydb, txtSql)
+    
+    data = fetch(rs, n=-1)
+    print(data) 
 }
+  
 
 
 
